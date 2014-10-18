e99f2f7857f7b6ce5ba8ae786c6c44f4210c4a08
bambanapt <bambanapt@hotmail.com>
2014-10-18 18:22:59 +0700

    finish

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -2,51 +2,117 @@ require "spec_helper"
 require "game"
 
 describe Game do
-	it 'can roll a strike at frame10' do
-		g = Game.new
+	let (:g1) { g1 = Game .new }
+    def roll_many_times(n, pins)
+		n.times do
+			g1.roll(pins)
+    	end
+    end
+	it 'can roll a perfect game' do
+		roll_many_times(12, 10)
+		expect(g1.score).to eq(300)
+	end
+
+	it 'can roll a spare at last frame' do
+		g2 = Game.new
 	    #frame1
-	    g.roll(5)
-	    g.roll(5) #spare
+	    g2.roll(5)
+	    g2.roll(5) #spare
 	    #frame2
-	    g.roll(3)
-	    g.roll(4)
+	    g2.roll(3)
+	    g2.roll(4)
 	    #frame3
-	    g.roll(10) #strike
+	    g2.roll(10) #strike
 	    #frame4
-	    g.roll(5)
-	    g.roll(3)
+	    g2.roll(5)
+	    g2.roll(3)
 	    #frame5
-	    g.roll(7)
-	    g.roll(3) #spare
+	    g2.roll(7)
+	    g2.roll(3) #spare
 	    #frame6
-	    g.roll(10) #strike
+	    g2.roll(10) #strike
 	    #frame7
-	    g.roll(8)
-	    g.roll(2) #spare
+	    g2.roll(8)
+	    g2.roll(2) #spare
 	    #frame8
-	    g.roll(2)
-	    g.roll(2)
+	    g2.roll(2)
+	    g2.roll(2)
 	    #frame9
-	    g.roll(4)
-	    g.roll(6) #spare
+	    g2.roll(4)
+	    g2.roll(6) #spare
 	    #frame10
-	    g.roll(10)#strike
-	    g.roll(2)
-	    g.roll(5) #row3
-	    expect(g.score).to eq(139)
+	    g2.roll(8)
+	    g2.roll(2) #spare
+	    g2.roll(5) #row3
+	    expect(g2.score).to eq(135)
     end
 
-    let (:g1) { g1 = Game .new }
-    def roll_many(n, pins)
-     n.times do
-       g1.roll(pins)
-     end
+	it 'can roll a strike at last frame' do
+		g3 = Game.new
+	    #frame1
+	    g3.roll(4)
+	    g3.roll(1)
+	    #frame2
+	    g3.roll(3)
+	    g3.roll(2)
+	    #frame3
+	    g3.roll(4)
+	    g3.roll(6) #spare
+	    #frame4
+	    g3.roll(10) #strike
+	    #frame5
+	    g3.roll(5)
+	    g3.roll(5) #spare
+	    #frame6
+	    g3.roll(2)
+	    g3.roll(8) #strike
+	    #frame7
+	    g3.roll(2)
+	    g3.roll(1)
+	    #frame8
+	    g3.roll(2)
+	    g3.roll(8) #strike
+	    #frame9
+	    g3.roll(10)	#strike
+	    #frame10
+	    g3.roll(10) #strike
+	    g3.roll(6) 
+	    g3.roll(2) #row3
+	    expect(g3.score).to eq(141)
     end
-   it 'can roll a perfect game' do
-     roll_many(12, 10)
-     expect(g1.score).to eq(300)
-   end
 
+    it 'can roll no spare or strike at last frame' do
+    	g4 = Game.new
+		#frame1
+		g4.roll(5)
+		g4.roll(1) 
+		#frame2
+		g4.roll(3)
+		g4.roll(4)
+		#frame3
+		g4.roll(10) #strike
+		#frame4
+		g4.roll(5)
+		g4.roll(3)
+		#frame5
+		g4.roll(7)
+		g4.roll(3) #spare
+		#frame6
+		g4.roll(10) #strike
+		#frame7
+		g4.roll(8)
+		g4.roll(2) #spare
+		#frame8
+		g4.roll(2)
+		g4.roll(2)
+		#frame9
+		g4.roll(4)
+		g4.roll(6) #spare
+		#frame10
+		g4.roll(2)
+		g4.roll(2)
+		#no row3
+		expect(g4.score).to eq(111)
+	end
 
-   
 end
